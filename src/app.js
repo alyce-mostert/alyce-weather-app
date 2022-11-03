796a7a4a746907c2c81594bd91d54381b2a4efa5
Alyce Mostert <113119006+alyce-mostert@users.noreply.github.com>
2022-11-03 22:44:18 +0800

    Removed console logs

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.city;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.temperature.current
@@ -47,5 +46,4 @@ let apiKey = "oe33c8e8f0dc27bd608235t719ad314c";
 let units = "metric";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query={Perth}&key=${apiKey}&units=${units}`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
